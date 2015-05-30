91ca605647f95ca414e3f4803a6011782d64a9b4
Christian Lalanne <christian.lalanne@ichec.ie>
2015-05-30 16:30:30 +0100

    CHG: testing

--- phiq.sh
+++ phiq.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <time>" >&2
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <time> <project>" >&2
   exit 1
 fi
 
 time=$1
-qsub -I -q PhiQ -l nodes=1:ppn=20 -l walltime=$time -A ipcc
+proj=$2
+
+qsub -I -q PhiQ -l nodes=1:ppn=20 -l walltime=$time -A $proj
+
+
+
