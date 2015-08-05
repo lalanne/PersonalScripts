416658faea0ac3b179a56cad9b3b07fb1223103a
Christian Lalanne <christian.lalanne@ichec.ie>
2015-08-05 18:11:03 +0100

    ADD: implementation of all the options, need some improvement though

--- tm.sh
+++ tm.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
 echo ""
+echo "Tmux sessions in this machine: "
+echo ""
+
+sessions=( $(tmux ls | awk '{print $1}' | tr -d : | column -c 40) )
+
+sessionNumber=0
+for i in ${sessions[@]}
+do
+    echo "$sessionNumber) $i"
+    let sessionNumber=sessionNumber+1
+done 
+
+echo ""
 echo "What do you want to do?: "
 echo "1) Create" 
 echo "2) Close"
@@ -12,6 +25,32 @@ read -p "?:" option
 if [ "$option" == '1' ]; then
     read -p  "Enter name of the session: " sessionName
     tmux new -s $sessionName
+fi
+
+if [ "$option" == '2' ]; then
+
+    echo ""
+    echo "Tmux sessions in this machine: "
+    echo ""
+
+    sessions=( $(tmux ls | awk '{print $1}' | tr -d : | column -c 40) )
+
+    sessionNumber=0
+    for i in ${sessions[@]}
+    do
+        echo "$sessionNumber) $i"
+        let sessionNumber=sessionNumber+1
+    done 
+
+    echo ""
+
+    while true; do
+        read -p "Which session number would you like to close(-1 to quit):" session
+        if [ "$session" == '-1' ]; then
+            exit 0
+        fi
+        tmux kill-session -t ${sessions[$session]}
+    done
 fi
 
 if [ "$option" == '3' ]; then
